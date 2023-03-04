791fc6dd3a202aea37e3f37c5b44f07bb770243a
AkashTayal999 <akashtayal999@gmail.com>
2023-03-04 20:32:30 +0530

    Adding few more function while fetcing data from Json using JsonNode

--- API JsonNode Functions/ReadingFromJsonNode.java
+++ API JsonNode Functions/ReadingFromJsonNode.java
@@ -1,6 +1,7 @@
 package com.learning.api.jsonnode;
 
 import java.util.Iterator;
+import java.util.Map.Entry;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -14,7 +15,7 @@ public class ReadingFromJsonNode {
 	 * 			Add operations as per your need.
 	 */
 	public void traverseJson(JsonNode node) {
-		
+
 		if(node.isArray()) {
 			ArrayNode arrayNode = (ArrayNode) node;			
 			for(int index=0;index<arrayNode.size();index++) {
@@ -23,7 +24,7 @@ public class ReadingFromJsonNode {
 		}
 		else if(node.isObject()) {
 			Iterator<String> fieldNames= node.fieldNames();
-			
+
 			while(fieldNames.hasNext()) {
 				str = fieldNames.next();
 				System.out.println(str);  // -------Printing the Field --------- // 
@@ -31,10 +32,43 @@ public class ReadingFromJsonNode {
 			}
 		}
 		else {
-			System.out.println(node.asText()); // ------Printing the field values--------//
+			if(node != null && (!node.isNull()))   // Null check. If node has null values it will not print.
+				System.out.println(node.asText()); // ------Printing the field values--------//
 		}
-		
+
+	}
+
+
+	public void readingFieldNameAndValuesFromJsonNode(JsonNode node ) {
+
+		Iterator<Entry<String,JsonNode>> it = node.fields();
+		while(it.hasNext()) {
+
+			Entry<String,JsonNode> entry = it.next();
+			System.out.println(entry.getKey()+ "  : " + entry.getValue());
+		}
+	}
+
+	public void readingFieldValueFromJsonNode(JsonNode node ) {
+
+		Iterator<String> it = node.fieldNames();
+		while(it.hasNext()) {
+			String field= it.next();
+			System.out.println(node.get(field));
+		}
 	}
 	
+	public String getJsonNodeFieldAtPath(JsonNode node, String path) {
+		
+		String str;
+		str = node.at(path).asText();
+		
+		if(str.isEmpty() || str==null)
+			return "";
+		else 
+			return str;
 	
+		
+	}
+
 }
